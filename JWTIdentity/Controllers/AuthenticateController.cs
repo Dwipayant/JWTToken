d213ac90c8acba0dd346f9fa58b4b9186283508f
DwipayanT <dwi.tripathy@gmail.com>
2020-10-01 16:47:00 +0530

    Jwt Token for login

---
+++ JWTIdentity/Controllers/AuthenticateController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTIdentity.Data;
+using JWTIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace JWTIdentity.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthenticateController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        private IConfiguration _configuration;
+        public AuthenticateController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this._configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+
+                var authClaims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtToken:SecretKey"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JwtToken:Issuer"],
+                    audience: _configuration["JwtToken:Issuer"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ JWTIdentity/Data/SeedDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTIdentity.Data
+{
+    public class SeedDB
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+            if (!context.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = "dwi.tripathy@gmail.com",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "Dwipayan"
+                };
+                userManager.CreateAsync(user, "Dwipayan@123");
+            }
+        }
+    }
+}
+
